a4709b249abf0d51b47854d8e63ddfb3426967ea
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-06-11 13:06:16 -0300

    finish

--- packages/server/src/modules/appointments/services/ListProvidersService.ts
+++ packages/server/src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacherProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -30,7 +31,10 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
